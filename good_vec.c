83e54ada144c2d14ea6b04e73e2523d4d9528645
benwernicke <ben.wernicke@student.hpi.uni-potsdam.de>
2021-11-16 00:25:33 +0100

    byte* array that stores own orgvals

--- good_vec.c
+++ good_vec.c
@@ -1,26 +1,39 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-size_t* vec_create()
+typedef char byte;
+
+void* vec_create()
 {
-    size_t* vec_wrapper = (size_t*)malloc(4 * sizeof(size_t));
-    vec_wrapper[0] = 0; // elements in buffer
-    vec_wrapper[1] = 1; // chunks allocated
-    return &vec_wrapper[2];
+    byte* vec_buffed = (byte*)malloc(2 * sizeof(size_t) + 1);
+    ((size_t*)vec_buffed)[0] = 0;
+    ((size_t*)vec_buffed)[1] = 1;
+    return &vec_buffed[2 * sizeof(size_t)];
 }
 
-inline size_t* vec_wrapper(void* vec)
+byte* vec_wrapper(void* vec)
 {
-    return ((size_t*)vec - 2);
+    return (vec - (2 * sizeof(size_t) + 1));
 }
 
-void vec_push(void*)
-
-    int main(void)
+void vec_free(void* vec)
 {
-    int* vec = (int*)vec_create();
-    vec[0] = 69;
-    vec[1] = 420;
+    free(vec_wrapper(vec));
+    vec = NULL;
+}
+
+void* vec_push(void* vec, char val)
+{
+    byte* vec_buffed = vec_wrapper(vec);
+    if (((size_t*)vec_buffed)[0] >= ((size_t*)vec_buffed)[1])
+        vec_buffed = (byte*)realloc(vec_buffed, ((size_t*)vec_buffed)[1] * 2);
+}
+
+int main(void)
+{
+    char* vec = vec_create();
+    vec_free(vec);
     return 0;
 }
