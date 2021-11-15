44635c8dda5f7adfc9e0593d81bd2f9f1e9f3781
benwernicke <ben.wernicke@student.hpi.uni-potsdam.de>
2021-11-15 15:01:01 +0100

    vec_push, vec_resize

--- bad_vec.c
+++ bad_vec.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #pragma pack(1)
 
 typedef unsigned char byte;
@@ -11,7 +12,7 @@ void* vec_create()
 
     vec_wrapper[0] = malloc(1 * sizeof(size_t));
     vec_wrapper[1] = malloc(1 * sizeof(size_t));
-    vec_wrapper[2] = malloc(4 * sizeof(byte));
+    vec_wrapper[2] = malloc(1 * sizeof(byte));
 
     *(size_t*)vec_wrapper[0] = 0; // size of array in elements
     *(size_t*)vec_wrapper[1] = 1; // actual allocated number of bytes
@@ -23,11 +24,31 @@ void** vec_wrapper_address(void* vec_buffer)
     return ((void**)vec_buffer) - 2;
 }
 
+// TODO: this is shit
+
+void vec_resize(void* vec, size_t n_bytes)
+{
+    void** vec_wrapper = vec_wrapper_address(vec);
+    /*vec_wrapper[2] = realloc(&vec_wrapper[2], n_bytes);*/
+    /**(size_t*)vec_wrapper[1] = n_bytes;*/
+    void** new_vec_wrapper = (void**)malloc(3 * sizeof(void*));
+    new_vec_wrapper[0] = malloc(1 * sizeof(size_t));
+    new_vec_wrapper[1] = malloc(1 * sizeof(size_t));
+    new_vec_wrapper[2] = malloc(1 * sizeof(byte));
+
+    *(size_t*)new_vec_wrapper[0] = *(size_t*)vec_wrapper[0]; // size of array in elements
+    *(size_t*)new_vec_wrapper[1] = n_bytes;                  // actual allocated number of bytes
+    new_vec_wrapper[2] = malloc(n_bytes);
+    memcpy(new_vec_wrapper[2], vec_wrapper[2], n_bytes < *(size_t*)vec_wrapper[1] ? n_bytes : *(size_t*)vec_wrapper[1]);
+    vec = &new_vec_wrapper[2];
+}
+
 void vec_push(void* vec, char val)
 {
     void** vec_wrapper = vec_wrapper_address(vec);
-    ((char*)vec)[*(size_t*)vec_wrapper[0]] = val;
-    (*(size_t*)vec_wrapper[0])++;
+    if (*(size_t*)vec_wrapper[0] >= *(size_t*)vec_wrapper[1])
+        vec_resize(vec, *(size_t*)vec_wrapper[1] * 2 + 1);
+    ((char*)vec)[(*(size_t*)vec_wrapper[0])++] = val;
 }
 
 int main()
